e0169e7e13800ab219cbcc0999488b41199c004f
Nicolas DUBIEN <github@dubien.org>
2014-07-04 05:57:35 +0200

    Sort is already done Facebook-side

--- js/scores.js
+++ js/scores.js
@@ -2,15 +2,11 @@ var uid = "";
 var current_best_score = 0;
 var all_scores = new Array();
 
-function sortByScore(a, b) {
-	return a.score > b.score;
-}
-
 function getScores() {
 	FB.api('/me?fields=id', function(response) {
 		uid = response.id;
 		FB.api('/1429099524039083/scores?fields=score', function(response) {
-			all_scores = response.data.sort(sortByScore);
+			all_scores = response.data;
 			var highscores_div = document.getElementById("highscores");
 			highscores_div.innerHTML = "";
 			for (var i=0 ; i!=all_scores.length ; i++) {
